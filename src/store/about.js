42d58445bbaddae40df6cee4f93cd01ea6560f17
ohtaekwon <ohtk1119@gmail.com>
2022-05-11 19:41:46 +0900

    about

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+  namespaced:true,
+
+  state : ()=>({
+    name : 'OHTAEKWON',
+    email: 'ohtk1119@gmail.com',
+    blog : 'https://github.com/ohtaekwon/TIL/tree/master/Vue-Movie-Project',
+    phone : '+082-10-1234-5678',
+    image : 'https://raw.githubusercontent.com/ohtaekwon/VUE3-MOVIE-APP/master/src/assets/logo.png'
+  }),
+}
